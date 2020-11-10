8682332c94024d3904c9119f355f7608bfff9ba1
Igor Tullio <igortullio@hotmail.com>
2020-11-10 13:46:42 -0300

    Add template Auth

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+
+import Logo from 'components/Logo'
+import Heading from 'components/Heading'
+
+import * as S from './styles'
+
+export type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
+  <S.Wrapper>
+    <S.BannerBlock>
+      <S.BannerContent>
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+
+        <div>
+          <Heading size="huge">All your favorite games in one place</Heading>
+          <S.SubTitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.SubTitle>
+        </div>
+
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
+    </S.BannerBlock>
+
+    <S.Content>
+      <S.ContentWrapper>
+        <Link href="/">
+          <a>
+            <Logo id="content" color="black" size="large" />
+          </a>
+        </Link>
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </S.ContentWrapper>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default Auth
